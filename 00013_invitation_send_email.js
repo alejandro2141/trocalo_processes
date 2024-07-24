eb0c89800b902e289fb867d0e089390594f5059a
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-07-24 12:11:18 -0400

    Update 00013_invitation_send_email.js

--- 00013_invitation_send_email.js
+++ 00013_invitation_send_email.js
@@ -136,7 +136,7 @@ async function sendmail(data)
         transporter.sendMail(
           {            
             //from: "Registro_Horapo_"+Math.floor(Math.random()* (1000 - 1) + 1)+"@horapo.com",
-            from: "Horapo_Registro@horapo.com",
+            from: "noreply@reusar.cl",
             to: data.email.toLowerCase()  ,
             subject: 'Un amigo te Invita a REUSAR.CL',
             html: data.message ,
